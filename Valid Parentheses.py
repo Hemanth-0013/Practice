d6ed5bdedc1c3f1359373033bcfc644614a06ba2
Hemanth P <154998501+Hemanth-0013@users.noreply.github.com>
2026-03-06 13:36:30 +0530

    Add valid parentheses checker
    Implement a solution to check for valid parentheses using a stack.

---
+++ Valid Parentheses.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isValid(self, s):
+        pairs = { 
+            ')' : '(', 
+            '}': '{' , 
+            ']' : '[' }
+        stack = []
+
+        for char in s:
+            if char in pairs:
+                if stack and stack[-1] == pairs[char]:
+                    stack.pop()
+                    
+                else:
+                    return False
+
+            else:
+                stack.append(char)
+
+        return len(stack) == 0
+
+
+        
